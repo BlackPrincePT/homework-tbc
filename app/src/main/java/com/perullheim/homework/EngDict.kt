b322d271458970e65ab7fa1da78ea6259161034d
Petre Chkonia <chkoniapetre@gmail.com>
2024-12-05 19:21:34 +0400

    Homework 3 + Bonus

---
+++ app/src/main/java/com/perullheim/homework/EngDict.kt
@@ -0,0 +1,53 @@
+package com.perullheim.homework
+
+class EngDict {
+    companion object {
+        val base100: Map<Int, String> = mapOf(
+            1 to "one",
+            2 to "two",
+            3 to "three",
+            4 to "four",
+            5 to "five",
+            6 to "six",
+            7 to "seven",
+            8 to "eight",
+            9 to "nine",
+            10 to "ten",
+            11 to "eleven",
+            12 to "twelve",
+            13 to "thirteen",
+            14 to "fourteen",
+            15 to "fifteen",
+            16 to "sixteen",
+            17 to "seventeen",
+            18 to "eighteen",
+            19 to "nineteen",
+            20 to "twenty",
+            30 to "thirty",
+            40 to "forty",
+            50 to "fifty",
+            60 to "sixty",
+            70 to "seventy",
+            80 to "eighty",
+            90 to "ninety"
+        )
+
+        val base1000 = mapOf(
+            3 to "thousand",
+            6 to "million",
+            9 to "billion",
+            12 to "trillion",
+            15 to "quadrillion",
+            18 to "quintillion",
+            21 to "sextillion",
+            24 to "septillion",
+            27 to "octillion",
+            30 to "nonillion",
+            33 to "decillion",
+            36 to "undecillion",
+            39 to "duodecillion",
+            42 to "tredecillion",
+            45 to "quattuordecillion"
+        )
+    }
+}
