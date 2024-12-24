583fe738f64ed06aaaf7dde7c4357a8a1c485186
BlackPrincePT <chkoniapetre@gmail.com>
2024-12-25 02:12:41 +0400

    Homework 10

---
+++ app/src/main/java/com/perullheim/homework/model/Category.kt
@@ -0,0 +1,11 @@
+package com.perullheim.homework.model
+
+enum class Category(val displayName: String) {
+    ALL("All"),
+    FAVOURITE("\u2764\uFE0F    Favorites"),
+    PARTY("\uD83C\uDF89    Party"),
+    CAMPING("\uD83C\uDFD5    Camping"),
+    CASUAL("\uD83D\uDC57    Casual"),
+    FORMAL("\uD83D\uDC8E    Formal"),
+    SPORT("\uD83E\uDDE4    Sport");
+}
