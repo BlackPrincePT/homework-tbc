a7e84afe3aee08903799ae62606c469786bdc975
BlackPrincePT <chkoniapetre@gmail.com>
2025-03-25 18:49:26 +0400

    Setup UI

---
+++ app/src/main/java/com/perullheim/homework/presentation/choose/ChooseFragment.kt
@@ -0,0 +1,9 @@
+package com.perullheim.homework.presentation.choose
+
+import com.perullheim.homework.databinding.FragmentChooseBinding
+import com.perullheim.homework.presentation.core.BaseBottomSheetDialogFragment
+
+class ChooseFragment :
+    BaseBottomSheetDialogFragment<FragmentChooseBinding>(FragmentChooseBinding::inflate) {
+
+}
---
+++ app/src/main/java/com/perullheim/homework/presentation/photo/PhotoFragment.kt
@@ -0,0 +1,8 @@
+package com.perullheim.homework.presentation.photo
+
+import com.perullheim.homework.databinding.FragmentPhotoBinding
+import com.perullheim.homework.presentation.core.BaseFragment
+
+class PhotoFragment : BaseFragment<FragmentPhotoBinding>(FragmentPhotoBinding::inflate) {
+
+}
