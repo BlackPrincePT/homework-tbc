e416b6e5cc421f154b7e5b3ef519888a5b6381be
BlackPrincePT <chkoniapetre@gmail.com>
2025-03-31 23:04:09 +0400

    Revert "Homework13"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         applicationId = "com.perullheim.homework"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/perullheim/homework/MainActivity.kt
+++ app/src/main/java/com/perullheim/homework/MainActivity.kt
@@ -5,25 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.perullheim.homework.view.ChatFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainContainer)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        launchFragment()
-    }
-
-    private fun launchFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.mainContainer, ChatFragment())
-            .commitNow()
     }
 }
