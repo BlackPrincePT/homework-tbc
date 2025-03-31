b1336c444647e8af77be781a16a7cf13a517d830
BlackPrincePT <chkoniapetre@gmail.com>
2025-03-31 23:02:47 +0400

    Revert "Homework12"

--- app/src/main/java/com/perullheim/homework/MainActivity.kt
+++ app/src/main/java/com/perullheim/homework/MainActivity.kt
@@ -5,31 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.perullheim.homework.databinding.ActivityMainBinding
-import com.perullheim.homework.view.my_orders.MyOrdersFragment
 
 class MainActivity : AppCompatActivity() {
-
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        launchMainFragment()
-    }
-
-    private fun launchMainFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.mainContainer, MyOrdersFragment())
-            .commitNow()
     }
 }
