bf4287b84d0903e5b9a6ab8d7d5dfdddc7c062f7
BlackPrincePT <chkoniapetre@gmail.com>
2025-04-05 01:03:07 +0400

    Revert "Homework23 compose"

--- app/src/main/java/com/perullheim/homework/domain/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.perullheim.homework.domain.repository
-
-import com.perullheim.homework.domain.model.User
-import kotlinx.coroutines.flow.Flow
-
-interface UserRepository {
-    fun fetchUsersStream(): Flow<List<User>>
-}
--- app/src/main/java/com/perullheim/homework/presentation/screen/register/RegisterUiEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.perullheim.homework.presentation.screen.register
-
-sealed interface RegisterUiEvent {
-    data class OnEmailChange(val value: String) : RegisterUiEvent
-    data class OnUsernameChange(val value: String) : RegisterUiEvent
-    data class OnPasswordChange(val value: String) : RegisterUiEvent
-    data object OnRegisterClick : RegisterUiEvent
-}
