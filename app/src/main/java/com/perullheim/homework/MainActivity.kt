85b3daa4bc707cfec5ae49e2396d59f71678a1b0
Petre Chkonia <chkoniapetre@gmail.com>
2025-04-11 11:51:45 +0400

    Revert "Homework15"

--- app/src/main/java/com/perullheim/homework/MainActivity.kt
+++ app/src/main/java/com/perullheim/homework/MainActivity.kt
@@ -2,19 +2,16 @@ package com.perullheim.homework
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.perullheim.homework.view.payment.PaymentViewModel
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragmentContainerView)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
